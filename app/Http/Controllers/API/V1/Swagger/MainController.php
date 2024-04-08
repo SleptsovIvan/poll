5f7b3bc1ce1e3f9ffb51487d2e0536f5e7b19a3b
SleptsovIvan <sl.vanya84@gmail.com>
2024-04-09 02:20:49 +0900

    swagger

---
+++ app/Http/Controllers/API/V1/Swagger/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Swagger;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="Poll API"
+ * )
+ * 
+ *  @OA\PathItem(path="/api")
+ */
+
+class MainController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/API/V1/Swagger/PollController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Swagger;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ *
+ * @OA\Post(
+ *      path="/api/admin/polls",
+ *      summary="Создание опроса",
+ *      tags={"Poll"},
+ *      @OA\RequestBody(
+ *          @OA\JsonContent(
+ *              allOf={
+ *                  @OA\Schema(
+ *                      @OA\Property(property="question", type="string", example="Some question"),
+ *                      @OA\Property(property="options", type="array", @OA\Items(
+ *                          @OA\Property(property="value", example="Some option", oneOf={
+ *                     	        @OA\Schema(type="string"),
+ *                     	        @OA\Schema(type="integer"),
+ *                          }))
+ *                      ),
+ *                  ),
+ *              },
+ *          ),
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="success", type="boolean", example="true"),
+ *          ),
+ *      ),
+ * ),
+ * 
+ * @OA\Get(
+ *      path="/api/admin/polls/{id}",
+ *      summary="Опрос",
+ *      tags={"Poll"},
+ *      @OA\Parameter(
+ *          description="ID опроса",
+ *          in="path",
+ *          name="id",
+ *          required=true,
+ *          example="69a6c62c-19cf-4977-bd96-ca4064ade088"
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(  
+ *              @OA\Property(property="data", type="array", @OA\Items(
+ *                  @OA\Property(property="id", type="string", format="uuid", example="69a6c62c-19cf-4977-bd96-ca4064ade088"),
+ *                  @OA\Property(property="type", type="string", example="poll"),
+ *                  @OA\Property(property="question", type="string", example="Some question"),
+ *                  @OA\Property(property="options", type="array", @OA\Items(
+ *                      @OA\Property(property="id", type="string", format="uuid", example="69a6c62c-19cf-4977-bd96-ca4064ade088"),
+ *                      @OA\Property(property="count", type="integer", example="0"),
+ *                      @OA\Property(property="value", example="Some option", oneOf={
+ *                     	    @OA\Schema(type="string"),
+ *                     	    @OA\Schema(type="integer"),
+ *                      }))
+ *                  ),
+ *              )),
+ *          ),
+ *      ),
+ * ),
+ * 
+ *  @OA\Put(
+ *      path="/api/admin/polls/{id}",
+ *      summary="Обновить опрос",
+ *      tags={"Poll"},
+ *      @OA\Parameter(
+ *          description="ID опроса",
+ *          in="path",
+ *          name="id",
+ *          required=true,
+ *          example="69a6c62c-19cf-4977-bd96-ca4064ade088"
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="success", type="boolean", example="true"),
+ *          ),
+ *      ),
+ * ),
+ * 
+ * @OA\Get(
+ *      path="/api/admin/polls",
+ *      summary="Список опросов",
+ *      tags={"Poll"},
+ *      @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(  
+ *              @OA\Property(property="data", type="array", @OA\Items(
+ *                  @OA\Property(property="id", type="string", format="uuid", example="69a6c62c-19cf-4977-bd96-ca4064ade088"),
+ *                  @OA\Property(property="type", type="string", example="poll"),
+ *                  @OA\Property(property="question", type="string", example="Some question"),
+ *                  @OA\Property(property="options", type="array", @OA\Items(
+ *                      @OA\Property(property="id", type="string", format="uuid", example="69a6c62c-19cf-4977-bd96-ca4064ade088"),
+ *                      @OA\Property(property="count", type="integer", example="0"),
+ *                      @OA\Property(property="value", example="Some option", oneOf={
+ *                     	    @OA\Schema(type="string"),
+ *                     	    @OA\Schema(type="integer"),
+ *                      }))
+ *                  ),
+ *              )),
+ *          ),
+ *      ),
+ * ),
+ * 
+ *  @OA\Delete(
+ *      path="/api/admin/polls/{id}",
+ *      summary="Удаление опроса",
+ *      tags = {"Poll"},
+ *      @OA\Parameter(
+ *          description="ID опроса",
+ *          in="path",
+ *          name="id",
+ *          required=true,
+ *          example="69a6c62c-19cf-4977-bd96-ca4064ade088"
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Ok",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="success", type="boolean", example="true"),
+ *          ),
+ *      ),
+ * ),
+ */
+
+class PollController extends Controller
+{
+    //
+}
